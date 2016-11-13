a8e8412477599eb2bb6821cec7ab265d433b9cbb
newman <newman@newmantekiiMac.local>
2016-11-13 23:53:58 +0800

    首页接口上传

---
+++ CommonFiles_ST/Controller/ST_Home/viewModel/LBB_Public.h
@@ -0,0 +1,12 @@
+//
+//  LBB_Public.h
+//  ST_Travel
+//
+//  Created by newman on 16/11/13.
+//  Copyright © 2016年 GL_RunMan. All rights reserved.
+//
+
+///当前屏幕rect
+
+#define BASEURL @"http://7d86792c.ittun.com/lbb"
+
