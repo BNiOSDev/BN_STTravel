036e2ab89712cdec05c361e62f1392d92250806a
newman <xuweikai@xuweikaideMacBook-Pro.local>
2016-11-20 14:57:26 +0800

    景点美食民宿接口

---
+++ CommonFiles_ST/Controller/ST_Home/ChildController/scenic/viewModel/LBB_ScenicViewModel.m
@@ -0,0 +1,220 @@
+//
+//  LBB_ScenicMainViewModel.m
+//  ST_Travel
+//
+//  Created by 许为锴 on 2016/11/19.
+//  Copyright © 2016年 GL_RunMan. All rights reserved.
+//
+
+#import "LBB_ScenicViewModel.h"
+
+@implementation LBB_ScenicSpotConditionOption
+
+@end
+
+@implementation LBB_ScenicSpotCondition
+
+- (void)setTags:(NSMutableArray *)tags
+{
+    NSMutableArray *array = (NSMutableArray *)[tags map:^id(NSDictionary *element) {
+        return [LBB_ScenicSpotConditionOption mj_objectWithKeyValues:element];
+    }];
+    _tags = array;
+}
+
+- (void)setType:(NSMutableArray *)type
+{
+    NSMutableArray *array = (NSMutableArray *)[type map:^id(NSDictionary *element) {
+        return [LBB_ScenicSpotConditionOption mj_objectWithKeyValues:element];
+    }];
+    _type = array;
+}
+
+- (void)setOrder:(NSMutableArray *)order
+{
+    NSMutableArray *array = (NSMutableArray *)[order map:^id(NSDictionary *element) {
+        return [LBB_ScenicSpotConditionOption mj_objectWithKeyValues:element];
+    }];
+    _order = array;
+}
+
+- (void)setPrice:(NSMutableArray *)price
+{
+    NSMutableArray *array = (NSMutableArray *)[price map:^id(NSDictionary *element) {
+        return [LBB_ScenicSpotConditionOption mj_objectWithKeyValues:element];
+    }];
+    _price = array;
+}
+
+- (void)setHotRecommend:(NSMutableArray *)hotRecommend
+{
+    NSMutableArray *array = (NSMutableArray *)[hotRecommend map:^id(NSDictionary *element) {
+        return [LBB_ScenicSpotConditionOption mj_objectWithKeyValues:element];
+    }];
+    _hotRecommend = array;
+}
+
+@end
+
+@implementation LBB_ScenicViewModel
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.scenicSpotCondition = [[LBB_ScenicSpotCondition alloc]init];
+        self.spotArray = [[NSMutableArray alloc]initFromNet];
+        self.advertisementArray = [[NSMutableArray alloc]initFromNet];
+    }
+    return self;
+}
+
+/**
+ 3.2.1	景点筛选条件(已测)
+ */
+- (void)getSpotCondition
+{    
+    NSString *url = [NSString stringWithFormat:@"%@/spot/condition",BASEURL];
+    __weak typeof(self) temp = self;
+    self.scenicSpotCondition.loadSupport.loadEvent = NetLoadingEvent;
+    
+    [[BC_ToolRequest sharedManager] GET:url parameters:nil success:^(NSURLSessionDataTask *operation, id responseObject) {
+        NSDictionary *dic = responseObject;
+        NSNumber *codeNumber = [dic objectForKey:@"code"];
+        if(codeNumber.intValue == 0)
+        {
+            [temp.scenicSpotCondition mj_setKeyValues:[dic objectForKey:@"result"]];
+        }
+        else
+        {
+            NSString *errorStr = [dic objectForKey:@"remark"];
+            NSLog(@"失败  %@",errorStr);
+            
+        }
+        
+        temp.scenicSpotCondition.loadSupport.loadEvent = codeNumber.intValue;
+    } failure:^(NSURLSessionDataTask *operation, NSError *error) {
+        temp.scenicSpotCondition.loadSupport.loadEvent = NetLoadFailedEvent;
+    }];
+}
+
+/**
+ 3.2.4	景点列表(已测)
+ 
+ @param longitude Y坐标
+ @param dimensionality Y坐标
+ @param typeKey 传入景点类型key
+ @param orderKey 传入排序key
+ @param hotRecommendKey 热门推荐key
+ @param tagsKey 标签key
+ @param priceKey 价格Key
+ */
+- (void)getSpotArrayLongitude:(NSString *)longitude
+               dimensionality:(NSString *)dimensionality
+                      typeKey:(int)typeKey
+                     orderKey:(int)orderKey
+              hotRecommendKey:(int)hotRecommendKey
+                      tagsKey:(int)tagsKey
+                     priceKey:(int)priceKey
+                    clearData:(BOOL)clear;
+{
+        int curPage = clear == YES ? 0 : round(self.spotArray.count/10.0);
+        NSDictionary *paraDic = @{
+                                  @"longitude":longitude,
+                                  @"dimensionality":dimensionality,
+                                  @"typeKey":@(typeKey),
+                                  @"orderKey":@(orderKey),
+                                  @"hotRecommendKey":@(hotRecommendKey),
+                                  @"tagsKey":@(tagsKey),
+                                  @"priceKey":@(priceKey),
+                                  @"curPage":[NSNumber numberWithInt:curPage],
+                                  @"pageNum":[NSNumber numberWithInt:10],
+                                  };
+    
+    NSString *url = [NSString stringWithFormat:@"%@/spot/list",BASEURL];
+    __weak typeof(self) temp = self;
+    self.spotArray.loadSupport.loadEvent = NetLoadingEvent;
+    
+    [[BC_ToolRequest sharedManager] GET:url parameters:paraDic success:^(NSURLSessionDataTask *operation, id responseObject) {
+        NSDictionary *dic = responseObject;
+        NSNumber *codeNumber = [dic objectForKey:@"code"];
+        if(codeNumber.intValue == 0)
+        {
+            NSLog(@"getUgcArrayClearData成功  %@",[dic objectForKey:@"rows"]);
+            NSArray *array = [dic objectForKey:@"rows"];
+            NSArray *returnArray = [LBB_SpotModel mj_objectArrayWithKeyValuesArray:array];
+            
+            if (clear == YES)
+            {
+                [temp.spotArray removeAllObjects];
+            }
+            
+            [temp.spotArray addObjectsFromArray:returnArray];
+            temp.spotArray.networkTotal = [dic objectForKey:@"total"];
+        }
+        else
+        {
+            NSString *errorStr = [dic objectForKey:@"remark"];
+        }
+        
+        temp.spotArray.loadSupport.loadEvent = codeNumber.intValue;
+    } failure:^(NSURLSessionDataTask *operation, NSError *error) {
+        NSLog(@"getUgcArrayClearData失败  %@",error.domain);
+        
+        temp.spotArray.loadSupport.loadEvent = NetLoadFailedEvent;
+    }];
+}
+
+/**
+ 3.1.2 广告轮播 4.景点页面最顶部
+ 
+ @param clear 是否清空原数据
+ */
+- (void)getAdvertisementListArrayClearData:(BOOL)clear
+{
+    //    int curPage = clear == YES ? 0 : round(self.advertisementArray.count/10.0);
+    //    NSDictionary *paraDic = @{
+    //                              @"curPage":[NSNumber numberWithInt:curPage],
+    //                              @"pageNum":[NSNumber numberWithInt:10],
+    //                              };
+    NSDictionary *paraDic = @{
+                              @"position":@(4)
+                              };
+    
+    NSString *url = [NSString stringWithFormat:@"%@/homePage/advertisementList",BASEURL];
+    __weak typeof(self) temp = self;
+    self.advertisementArray.loadSupport.loadEvent = NetLoadingEvent;
+    
+    [[BC_ToolRequest sharedManager] GET:url parameters:paraDic success:^(NSURLSessionDataTask *operation, id responseObject) {
+        NSDictionary *dic = responseObject;
+        NSNumber *codeNumber = [dic objectForKey:@"code"];
+        if(codeNumber.intValue == 0)
+        {
+            NSLog(@"getAdvertisementListArrayClearData成功  %@",[dic objectForKey:@"rows"]);
+            NSArray *array = [dic objectForKey:@"rows"];
+            NSArray *returnArray = [LBB_SportAdvertisement mj_objectArrayWithKeyValuesArray:array];
+            
+            if (clear == YES)
+            {
+                [temp.advertisementArray removeAllObjects];
+            }
+            
+            [temp.advertisementArray addObjectsFromArray:returnArray];
+            temp.advertisementArray.networkTotal = [dic objectForKey:@"total"];
+        }
+        else
+        {
+            NSString *errorStr = [dic objectForKey:@"remark"];
+            NSLog(@"失败  %@",errorStr);
+            
+        }
+        
+        temp.advertisementArray.loadSupport.loadEvent = codeNumber.intValue;
+    } failure:^(NSURLSessionDataTask *operation, NSError *error) {
+        NSLog(@"getAdvertisementListArrayClearData失败  %@",error.domain);
+        
+        temp.advertisementArray.loadSupport.loadEvent = NetLoadFailedEvent;
+    }];
+}
+
+@end
---
+++ CommonFiles_ST/Controller/ST_Home/viewModel/LBB_SpotDetailsViewModel.m
@@ -0,0 +1,231 @@
+//
+//  LBB_SpotDetailsViewModel.m
+//  ST_Travel
+//
+//  Created by 许为锴 on 2016/11/20.
+//  Copyright © 2016年 GL_RunMan. All rights reserved.
+//
+
+#import "LBB_SpotDetailsViewModel.h"
+
+@implementation LBB_SpotsNearbyRecommendData
+
+- (void)setTags:(NSMutableArray<LBB_SpotsTag *> *)tags
+{
+    NSMutableArray *array = (NSMutableArray *)[tags map:^id(NSDictionary *element) {
+        return [LBB_SpotsTag mj_objectWithKeyValues:element];
+    }];
+    _tags = array;
+}
+
+@end
+
+@implementation LBB_SpotsCommentsRecord
+
+@end
+
+@implementation LBB_SpotsCollectedRecord
+
+@end
+
+@implementation LBB_SpotsFacilities
+
+@end
+
+@implementation LBB_SpotsTag
+
+@end
+
+@implementation LBB_SpotsUgc
+
+@end
+
+@implementation LBB_PurchaseRecords
+
+@end
+
+@implementation LBB_SpotsPics
+
+@end
+
+@implementation LBB_SpotDetailsViewModel
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.nearbyRecommends = [[NSMutableArray alloc]initFromNet];
+    }
+    return self;
+}
+
+- (void)setAllSpotsPics:(NSMutableArray<LBB_SpotsPics *> *)allSpotsPics
+{
+    NSMutableArray *array = (NSMutableArray *)[allSpotsPics map:^id(NSDictionary *element) {
+        return [LBB_SpotsPics mj_objectWithKeyValues:element];
+    }];
+    _allSpotsPics = array;
+}
+
+- (void)setPurchaseRecords:(NSMutableArray<LBB_PurchaseRecords *> *)purchaseRecords
+{
+    NSMutableArray *array = (NSMutableArray *)[purchaseRecords map:^id(NSDictionary *element) {
+        return [LBB_PurchaseRecords mj_objectWithKeyValues:element];
+    }];
+    _purchaseRecords = array;
+}
+
+- (void)setUgc:(NSMutableArray<LBB_SpotsUgc *> *)ugc
+{
+    NSMutableArray *array = (NSMutableArray *)[ugc map:^id(NSDictionary *element) {
+        return [LBB_SpotsUgc mj_objectWithKeyValues:element];
+    }];
+    _ugc = array;
+}
+
+- (void)setTags:(NSMutableArray<LBB_SpotsTag *> *)tags
+{
+    NSMutableArray *array = (NSMutableArray *)[tags map:^id(NSDictionary *element) {
+        return [LBB_SpotsTag mj_objectWithKeyValues:element];
+    }];
+    _tags = array;
+}
+
+- (void)setFacilities:(NSMutableArray<LBB_SpotsFacilities *> *)facilities
+{
+    NSMutableArray *array = (NSMutableArray *)[facilities map:^id(NSDictionary *element) {
+        return [LBB_SpotsFacilities mj_objectWithKeyValues:element];
+    }];
+    _facilities = array;
+}
+
+- (void)setCollectedRecord:(NSMutableArray<LBB_SpotsCollectedRecord *> *)collectedRecord
+{
+    NSMutableArray *array = (NSMutableArray *)[collectedRecord map:^id(NSDictionary *element) {
+        return [LBB_SpotsCollectedRecord mj_objectWithKeyValues:element];
+    }];
+    _collectedRecord = array;
+}
+
+- (void)setCommentsRecord:(NSMutableArray<LBB_SpotsCommentsRecord *> *)commentsRecord
+{
+    NSMutableArray *array = (NSMutableArray *)[commentsRecord map:^id(NSDictionary *element) {
+        return [LBB_SpotsCommentsRecord mj_objectWithKeyValues:element];
+    }];
+    _commentsRecord = array;
+}
+
+/**
+ 3.2.8	周边推荐(已测)
+ 
+ @param type 1.景点 2.美食 3.民宿
+ @param longitude Y坐标
+ @param dimensionality X坐标
+ @param clear 是否清空原数据
+ */
+- (void)getSpotNearbyRecommendsType:(int)type
+                          Longitude:(NSString*)longitude
+                     dimensionality:(NSString*)dimensionality
+                          clearData:(BOOL)clear
+{
+    int curPage = clear == YES ? 0 : round(self.nearbyRecommends.count/10.0);
+    NSDictionary *paraDic = @{
+                              @"longitude":longitude,
+                              @"dimensionality":dimensionality,
+                              @"type":@(type),
+                              @"curPage":[NSNumber numberWithInt:curPage],
+                              @"pageNum":[NSNumber numberWithInt:10],
+                              };
+    
+    NSString *url = [NSString stringWithFormat:@"%@/spot/nearbyRecommends",BASEURL];
+    __weak typeof(self) temp = self;
+    self.nearbyRecommends.loadSupport.loadEvent = NetLoadingEvent;
+    
+    [[BC_ToolRequest sharedManager] GET:url parameters:paraDic success:^(NSURLSessionDataTask *operation, id responseObject) {
+        NSDictionary *dic = responseObject;
+        NSNumber *codeNumber = [dic objectForKey:@"code"];
+        if(codeNumber.intValue == 0)
+        {
+            NSLog(@"getUgcArrayClearData成功  %@",[dic objectForKey:@"rows"]);
+            NSArray *array = [dic objectForKey:@"rows"];
+            NSArray *returnArray = [LBB_SpotsNearbyRecommendData mj_objectArrayWithKeyValuesArray:array];
+            
+            if (clear == YES)
+            {
+                [temp.nearbyRecommends removeAllObjects];
+            }
+            
+            [temp.nearbyRecommends addObjectsFromArray:returnArray];
+            temp.nearbyRecommends.networkTotal = [dic objectForKey:@"total"];
+        }
+        else
+        {
+            NSString *errorStr = [dic objectForKey:@"remark"];
+        }
+        
+        temp.nearbyRecommends.loadSupport.loadEvent = codeNumber.intValue;
+    } failure:^(NSURLSessionDataTask *operation, NSError *error) {
+        NSLog(@"getUgcArrayClearData失败  %@",error.domain);
+        
+        temp.nearbyRecommends.loadSupport.loadEvent = NetLoadFailedEvent;
+    }];
+}
+
+@end
+
+@implementation LBB_SpotModel
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.spotDetails = [[LBB_SpotDetailsViewModel alloc]init];
+    }
+    return self;
+}
+
+- (void)setTags:(NSArray<LBB_SpotsTag *> *)tags
+{
+    NSMutableArray *array = [@[] mutableCopy];
+    [tags enumerateObjectsUsingBlock:^(LBB_SpotsTag * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSDictionary *dic = (NSDictionary *)obj;
+        LBB_SpotsTag *tag = [LBB_SpotsTag mj_objectWithKeyValues:dic];
+        [array addObject:tag];
+    }];
+    _tags = array;
+}
+
+/**
+ 3.2.7	景点/美食/民宿详情(已测)
+ */
+- (void)getSpotDetailsData
+{
+    NSString *url = [NSString stringWithFormat:@"%@/spot/view/%ld",BASEURL,self.allSpotsId];
+    __weak typeof(self) temp = self;
+    self.spotDetails.loadSupport.loadEvent = NetLoadingEvent;
+    
+    [[BC_ToolRequest sharedManager] GET:url parameters:nil success:^(NSURLSessionDataTask *operation, id responseObject) {
+        NSDictionary *dic = responseObject;
+        NSNumber *codeNumber = [dic objectForKey:@"code"];
+        if(codeNumber.intValue == 0)
+        {
+            [temp.spotDetails mj_setKeyValues:[dic objectForKey:@"result"]];
+        }
+        else
+        {
+            NSString *errorStr = [dic objectForKey:@"remark"];
+            NSLog(@"失败  %@",errorStr);
+            
+        }
+        
+        temp.spotDetails.loadSupport.loadEvent = codeNumber.intValue;
+    } failure:^(NSURLSessionDataTask *operation, NSError *error) {
+        temp.spotDetails.loadSupport.loadEvent = NetLoadFailedEvent;
+    }];
+}
+
+@end
+
+@implementation LBB_SportAdvertisement
+
+@end
