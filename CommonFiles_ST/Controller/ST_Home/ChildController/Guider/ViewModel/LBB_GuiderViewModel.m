c86b8c483e7d74d137e5bb0e500fb23384cb26ca
柯尔祥 <keerxiang@keerxiangs-MacBook-Pro.local>
2016-12-10 11:04:39 +0800

    创建导游的模型文件

---
+++ CommonFiles_ST/Controller/ST_Home/ChildController/Guider/ViewModel/LBB_GuiderViewModel.m
@@ -0,0 +1,18 @@
+//
+//  LBB_GuiderListViewModel.m
+//  ST_Travel
+//
+//  Created by 柯尔祥 on 2016/12/10.
+//  Copyright © 2016年 GL_RunMan. All rights reserved.
+//
+
+#import "LBB_GuiderViewModel.h"
+
+@implementation LBB_GuiderListViewModel
+
+@end
+
+
+@implementation LBB_GuiderViewModel
+
+@end
